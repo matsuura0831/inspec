f516fdaaa173fef1088bb767441c885868289473
matsuura0831 <matsuura0831@gmail.com>
2020-04-22 18:09:33 +0900

    add check autoprotect status.

--- recipe/antivirus.rb
+++ recipe/antivirus.rb
@@ -5,5 +5,11 @@ control "Check antivirus software" do
     describe file('/opt/Symantec/symantec_antivirus/sav') do
       its('type') { should eq :file }
     end
+
+    describe command('/opt/Symantec/symantec_antivirus/sav info -a') do
+      its('stdout') { should eq "Enabled\n" }
+      its('stderr') { should eq '' }
+      its('exit_status') { should eq 0 }
+    end
   end
 end
